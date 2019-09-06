53e62fb5eeff73ccc5615c7fd2c88648adcb2e89
flashfan88 <lavarjohnson20@gmail.com>
2019-09-06 17:34:55 -0500

    itried

---
+++ retake.py
@@ -0,0 +1,86 @@
+# Ask a user for their first name and age. Print "[NAME] is [AGE] years old"
+
+# userinput = input("What is your name")
+# userinput2 = int(input("What is your Age"))
+# print(f"{userinput} is {userinput2} years old")
+
+# Ask the user to enter their age.
+# Check if they entered a value between 0 and 125. If valid, print "REAL AGE".
+# If invalid print “NOT A REAL AGE!!!”
+
+# userinput = int(input("Enter your Age"))
+# if 0 <= userinput <= 125:
+#     print("REAL AGE")
+# else:
+#     print("NOT A REAL AGE")
+
+# Use a for loop to print every 4 numbers from -50 to 50.
+
+# for x in range(-50, 50+4, 4):
+#     print(x)
+
+# Ask the user to enter a number to add to a total.
+# Keep asking the user to enter a number until they enter 0.
+# Afterward, print the total of all numbers entered.
+
+# userinput = ""
+# total = 10
+# while userinput != 0:
+#     userinput = int(input("Enter a number to add to the total"))
+#     print(total + userinput)
+
+# Create an array of 4 names.
+# Print one string that has all of the arrays separated by commas.
+
+# myarray = ["batman", "superman", "flash", "wonderwoman"]
+# for x in myarray:
+#     print(x)
+
+
+# Create a function that’s passed three integer numbers.
+# Print the sum of the first two numbers and return the product of the second two numbers.
+# def number(num):
+#     print()
+
+
+# Create the class Boardgame with name, price, pieceCount, and publisher properties/attributes.
+# Create a class method that will change the price of the book.
+# Outside of the class, create three objects of the class Boardgame.
+# Print the three board game objects using the newly created objects.
+
+# class boardGame:
+#     def __init__(new, name, price, pieceCount, publisher):
+#         new.name = name
+#         new.price = price
+#         new.pieceCount = pieceCount
+#         new.publisher = publisher
+
+
+# game1 = boardGame("Monopoly", 20.00, 50, "Hasbro")
+
+# print(game1.name)
+# print(game1.pieceCount)
+# print(game1.publisher)
+
+
+
+# Create a function that takes a string array and returns a string array
+# with the letter 's' at the end of each element. Call the function.
+
+# def stuff(string):
+#     for G in string:
+#         print(G + "s")
+
+# games = ["Super Mario", "Sonic the hedgehog", "Crash Bandicoot"]
+# stuff(games)
+
+# Create a function that has a parameter of an integer array and
+# returns only the positive numbers in the array. Call the function
+
+# def number():
+
+
+
+# Create a Puppy class. It should have properties name and color.
+# Create a program that will ask a user to enter the name,
+# then the color of a puppy until they enter 'q' to quit. Put each entry in an array.
